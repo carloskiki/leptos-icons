fd01fd91eb0202aabc31f3e73ddcb8a695d2e01d
Lukas Potthast <privat@lukas-potthast.de>
2023-03-26 15:12:42 +0200

    Update example; Testing case sensitive features and icon with leading number

--- leptos-icons/examples/simple-app/src/main.rs
+++ leptos-icons/examples/simple-app/src/main.rs
@@ -1,12 +1,19 @@
 use leptos::*;
-use leptos_icons::fa::solid::Bars::*;
+use leptos_icons::bs::*;
+use leptos_icons::fa::*;
+use leptos_icons::im::*;
 
 fn main() {
     _ = console_log::init_with_level(log::Level::Debug);
     console_error_panic_hook::set_once();
-    mount_to_body(|cx| view! { cx,
-        <div>
-            <Bars />
-        </div>
+    mount_to_body(|cx| {
+        view! { cx,
+            <div>
+                <Bs1Circle />
+                <FaBarsSolid />
+                <ImPagebreak />
+                <ImPageBreak />
+            </div>
+        }
     })
 }
