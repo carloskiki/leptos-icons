95faff5f0eb8d457d929f5e6a1c3206ec018075e
Charles Edward Gagnon <charlesedgagnon@gmail.com>
2023-04-23 08:18:05 -0400

    Example error

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@
 //! ```toml
 //! [dependencies]
 //! # ...
-//! leptos_icons = { git = "https://github.com/Carlosted/leptos-icons.git" features = ["BsFolder"] }
+//! leptos_icons = { version = "{crate_version}", features = ["BsFolder"] }
 //! ```
 //! In your leptos project, use:
 //! ```
