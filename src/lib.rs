3ca77deee02193096059f4de4a3d600cdbc25805
Charles Edward Gagnon <charlesedgagnon@gmail.com>
2023-04-22 21:18:43 -0400

    added lucide; upgraded every collection to 0.0.2

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,37 @@
 #[cfg(feature = "Ai")]
 pub use icondata_ai::AiIcon;
-#[cfg(feature = "Fa")]
-pub use icondata_fa::FaIcon;
-#[cfg(feature = "Wi")]
-pub use icondata_wi::WiIcon;
-#[cfg(feature = "Fi")]
-pub use icondata_fi::FiIcon;
-#[cfg(feature = "Vs")]
-pub use icondata_vs::VsIcon;
-#[cfg(feature = "Bs")]
-pub use icondata_bs::BsIcon;
 #[cfg(feature = "Bi")]
 pub use icondata_bi::BiIcon;
+#[cfg(feature = "Bs")]
+pub use icondata_bs::BsIcon;
+#[cfg(feature = "Cg")]
+pub use icondata_cg::CgIcon;
+#[cfg(feature = "Fa")]
+pub use icondata_fa::FaIcon;
+#[cfg(feature = "Fi")]
+pub use icondata_fi::FiIcon;
+#[cfg(feature = "Hi")]
+pub use icondata_hi::HiIcon;
 #[cfg(feature = "Im")]
 pub use icondata_im::ImIcon;
 #[cfg(feature = "Io")]
 pub use icondata_io::IoIcon;
+#[cfg(feature = "Oc")]
+pub use icondata_oc::OcIcon;
 #[cfg(feature = "Ri")]
 pub use icondata_ri::RiIcon;
 #[cfg(feature = "Si")]
 pub use icondata_si::SiIcon;
-#[cfg(feature = "Ti")]
-pub use icondata_ti::TiIcon;
-#[cfg(feature = "Hi")]
-pub use icondata_hi::HiIcon;
-#[cfg(feature = "Cg")]
-pub use icondata_cg::CgIcon;
 #[cfg(feature = "Tb")]
 pub use icondata_tb::TbIcon;
-#[cfg(feature = "Oc")]
-pub use icondata_oc::OcIcon;
+#[cfg(feature = "Ti")]
+pub use icondata_ti::TiIcon;
+#[cfg(feature = "Vs")]
+pub use icondata_vs::VsIcon;
+#[cfg(feature = "Wi")]
+pub use icondata_wi::WiIcon;
+#[cfg(feature = "Lu")]
+pub use icondata_lu::WiIcon;
 #[leptos::component]
 pub fn LeptosIcon(
     cx: leptos::Scope,
@@ -65,30 +67,24 @@ pub fn LeptosIcon(
     if let Some(y) = icon.y {
         svg = svg.attr("x", y);
     }
-    svg = svg
-        .attr(
-            "width",
-            leptos::Attribute::String(
-                match (width, icon.width) {
-                    (Some(a), Some(_b)) => a,
-                    (Some(a), None) => a,
-                    (None, Some(_b)) => "1em".to_owned(),
-                    (None, None) => "1em".to_owned(),
-                },
-            ),
-        );
-    svg = svg
-        .attr(
-            "height",
-            leptos::Attribute::String(
-                match (height, icon.height) {
-                    (Some(a), Some(_b)) => a,
-                    (Some(a), None) => a,
-                    (None, Some(_b)) => "1em".to_owned(),
-                    (None, None) => "1em".to_owned(),
-                },
-            ),
-        );
+    svg = svg.attr(
+        "width",
+        leptos::Attribute::String(match (width, icon.width) {
+            (Some(a), Some(_b)) => a,
+            (Some(a), None) => a,
+            (None, Some(_b)) => "1em".to_owned(),
+            (None, None) => "1em".to_owned(),
+        }),
+    );
+    svg = svg.attr(
+        "height",
+        leptos::Attribute::String(match (height, icon.height) {
+            (Some(a), Some(_b)) => a,
+            (Some(a), None) => a,
+            (None, Some(_b)) => "1em".to_owned(),
+            (None, None) => "1em".to_owned(),
+        }),
+    );
     if let Some(view_box) = icon.view_box {
         svg = svg.attr("viewBox", view_box);
     }
