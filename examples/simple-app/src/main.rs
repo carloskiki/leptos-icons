e35416fb3d990c8619d7fbf62f3cbef3a1b25311
Charles Edward Gagnon <charlesedgagnon@gmail.com>
2023-06-26 20:03:29 -0400

    added macro support; version 0.0.14

--- examples/simple-app/src/main.rs
+++ examples/simple-app/src/main.rs
@@ -7,8 +7,8 @@ fn main() {
     mount_to_body(|cx| {
         view! { cx,
             <div style="font-size: 8em; color: #8f39d3;">
-                <Icon icon=Icon::from(AiIcon::AiCarryOutTwotone) style="color: green" />
-                <Icon icon=Icon::from(BiIcon::BiGraphql) width="2em" height="2em" style="color: green"/>
+                <Icon icon=icon!(AiCarryOutTwotone) style="color: green" />
+                <Icon icon=icon!(BiGraphql) width="2em" height="2em" style="color: green"/>
                 <Icon icon=Icon::from(BiIcon::BiGraphql) style="color: orange"/>
                 <Icon icon=Icon::from(BsIcon::Bs1Circle) style="color: red"/>
                 <Icon icon=Icon::from(FaIcon::FaBarsSolid) />
