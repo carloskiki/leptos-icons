85e6f2c75be23e7025e696ab7ad96c3344633a42
Charles Edward Gagnon <charlesedgagnon@gmail.com>
2023-10-11 10:37:59 -0400

    Fixed simple-app example; added theme button example;

---
+++ examples/theme-button/src/main.rs
@@ -0,0 +1,48 @@
+use leptos::*;
+use leptos_icons::*;
+use leptos_meta::*;
+
+
+const DIV_STYLE: &str = r#"
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh;
+    margin: 0;
+    cursor: pointer;
+"#;
+
+const ICON_STYLE: &str = r#"
+    width: 20rem;
+    height: 20rem;
+    padding: 0.5rem;
+    border: 4px solid;
+    border-radius: 1rem;
+"#;
+
+
+pub fn main() {
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+    provide_meta_context();
+
+    let (dark, set_dark) = create_signal(false);
+    let icon = Signal::derive(move || {
+        if dark.get() {
+            Icon::from(BsMoonStars)
+        } else {
+            Icon::from(BsSun)
+        }
+    });
+    let toggle_theme = move |_| set_dark.update(|dark| *dark = !*dark);
+
+    mount_to_body(move || {
+        view! {
+            <Meta name="color-scheme" content=move || if dark.get() { "light".to_string() } else { "dark".to_string() } />
+            <div on:click=toggle_theme style=DIV_STYLE>
+                <Icon icon style=ICON_STYLE />
+            </div>
+        }
+    })
+}
